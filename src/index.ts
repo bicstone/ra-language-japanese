cda6513a9d2bba8a8b90da447a7dae9d5770e32c
Takanori Oishi <takanori0014@live.jp>
2022-10-09 01:22:51 +0900

    remove ra.configurable.templateError

--- src/index.ts
+++ src/index.ts
@@ -170,7 +170,6 @@ const messages: Required<TranslationMessages> = {
     },
     configurable: {
       customize: "カスタマイズ",
-      templateError: "## Template error",
       configureMode: "このページをカスタマイズする",
       inspector: {
         title: "カスタマイズ",
